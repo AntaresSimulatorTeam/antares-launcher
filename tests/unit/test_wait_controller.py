1034669f1ff8482b782a97bf71238339b468cb06
Andrea Sgattoni <andrea.sgattoni@rte-france.com>
2021-05-27 12:32:59 +0200

    refactor: change check_job_stats

--- tests/unit/test_wait_controller.py
+++ tests/unit/test_wait_controller.py
@@ -10,12 +10,12 @@ from antareslauncher.use_cases.wait_loop_controller.wait_controller import (
 
 class TestWaitController:
     @pytest.mark.unit_test
-    def test_countdown_calls_display_message_5_times_if_it_waits_2_seconds(
+    def test_countdown_calls_display_message_4_times_if_it_waits_1_seconds(
         self,
     ):
         display = IDisplay
         display.show_message = mock.Mock()
         display.show_message_no_newline = mock.Mock()
         wait_controller = WaitController(display)
-        wait_controller.countdown(2)
-        assert display.show_message.call_count == 5
+        wait_controller.countdown(1)
+        assert display.show_message.call_count == 4
