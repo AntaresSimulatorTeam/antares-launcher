7e38856c2e6836bcdd58a199c3dc1cb25321fcdf
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-02-10 21:58:43 +0100

    docs: update and correct the documentation.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-import recommonmark
+# import recommonmark
 from m2r import MdInclude
 
 # sys.path.insert(0, os.path.abspath('.'))
@@ -64,7 +64,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 
 # The name of the Pygments (syntax highlighting) style to use.
