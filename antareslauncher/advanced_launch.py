35eeb4df4c38009682ae946ea23461b1a64212f3
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-02-17 17:01:38 +0100

    build: correct the console script entry point to use `antareslauncher.advanced_launch.main`

--- antareslauncher/advanced_launch.py
+++ antareslauncher/advanced_launch.py
@@ -1,4 +1,3 @@
-import sys
 from pathlib import Path
 
 from antareslauncher.main import run_with, MainParameters
@@ -12,7 +11,8 @@ DATA_DIR = Path(__file__).parent.resolve() / "data"
 SSH_JSON_FILE = DATA_DIR / "sshconfig.json"
 YAML_CONF_FILE = DATA_DIR / "configuration.yaml"
 
-if __name__ == "__main__":
+
+def main():
     param_reader = ParametersReader(
         json_ssh_conf=SSH_JSON_FILE, yaml_filepath=YAML_CONF_FILE
     )
@@ -20,9 +20,9 @@ if __name__ == "__main__":
     parser: MainOptionParser = MainOptionParser(parser_parameters)
     parser.add_basic_arguments().add_advanced_arguments()
     arguments = parser.parse_args()
-
     main_parameters: MainParameters = param_reader.get_main_parameters()
-
     run_with(arguments=arguments, parameters=main_parameters, show_banner=True)
-    if not len(sys.argv) > 1:
-        input("Press ENTER to exit.")
+
+
+if __name__ == "__main__":
+    main()
--- antareslauncher/basic_launch.py
+++ antareslauncher/basic_launch.py
@@ -12,8 +12,8 @@ DATA_DIR = Path(__file__).parent.resolve() / "data"
 SSH_JSON_FILE = DATA_DIR / "sshconfig.json"
 YAML_CONF_FILE = DATA_DIR / "configuration.yaml"
 
-if __name__ == "__main__":
 
+def main():
     param_reader = ParametersReader(
         json_ssh_conf=SSH_JSON_FILE, yaml_filepath=YAML_CONF_FILE
     )
@@ -21,9 +21,9 @@ if __name__ == "__main__":
     parser: MainOptionParser = MainOptionParser(parser_parameters)
     parser.add_basic_arguments()
     arguments = parser.parse_args()
-
     main_parameters: MainParameters = param_reader.get_main_parameters()
-
     run_with(arguments=arguments, parameters=main_parameters, show_banner=True)
-    if not len(sys.argv) > 1:
-        input("Press ENTER to exit.")
+
+
+if __name__ == "__main__":
+    main()
