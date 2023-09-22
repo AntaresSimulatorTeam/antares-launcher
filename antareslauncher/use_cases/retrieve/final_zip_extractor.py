1ffc86e0439814e4549f59c193731c71080c0d59
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2023-09-22 16:33:24 +0200

    feat(zip-extractor): the uncompress directory is calculated according to the content: study directory or simulation output

--- antareslauncher/use_cases/retrieve/final_zip_extractor.py
+++ antareslauncher/use_cases/retrieve/final_zip_extractor.py
@@ -1,3 +1,4 @@
+import os.path
 import zipfile
 from pathlib import Path
 
@@ -21,15 +22,25 @@ class FinalZipExtractor:
         """
         if study.finished and not study.with_error and study.local_final_zipfile_path and not study.final_zip_extracted:
             zip_path = Path(study.local_final_zipfile_path)
-            target_dir = zip_path.with_suffix("")
             try:
                 with zipfile.ZipFile(zip_path) as zf:
                     names = zf.namelist()
+                    if len(names) > 1 and os.path.commonpath(names):
+                        # If all files are in the same directory, we can extract the ZIP
+                        # file directly in the target directory.
+                        target_dir = zip_path.parent
+                    else:
+                        # Otherwise, we need to create a directory to store the results.
+                        # This situation occurs when the ZIP file contains
+                        # only the simulation results and not the entire study.
+                        target_dir = zip_path.with_suffix("")
+
                     progress_bar = self._display.generate_progress_bar(
                         names, desc="Extracting archive:", total=len(names)
                     )
                     for file in progress_bar:
                         zf.extract(member=file, path=target_dir)
+
             except (OSError, zipfile.BadZipFile) as exc:
                 # If we cannot extract the final ZIP file, either because the file
                 # doesn't exist or the ZIP file is corrupted, we find ourselves
@@ -42,6 +53,7 @@ class FinalZipExtractor:
                     f'"{study.name}": Final zip not extracted: {exc}',
                     LOG_NAME,
                 )
+
             else:
                 study.final_zip_extracted = True
                 self._display.show_message(
--- tests/unit/retriever/test_final_zip_extractor.py
+++ tests/unit/retriever/test_final_zip_extractor.py
@@ -10,12 +10,26 @@ from antareslauncher.study_dto import StudyDTO
 from antareslauncher.use_cases.retrieve.final_zip_extractor import FinalZipExtractor
 
 
-def create_final_zip(study: StudyDTO, *, scenario: str = "nominal") -> str:
+def create_final_zip(study: StudyDTO, *, scenario: str = "nominal_study") -> str:
     """Prepare a final ZIP."""
     dst_dir = Path(study.output_dir)  # must exist
     dst_dir.mkdir(parents=True, exist_ok=True)
     out_path = dst_dir.joinpath(f"finished_{study.name}_{study.job_id}.zip")
-    if scenario == "nominal":
+    if scenario == "nominal_study":
+        with zipfile.ZipFile(
+            out_path,
+            mode="w",
+            compression=zipfile.ZIP_DEFLATED,
+        ) as zf:
+            zf.writestr(
+                f"{study.name}/input/study.antares",
+                data=b"[antares]\nversion = 860\n",
+            )
+            zf.writestr(
+                f"{study.name}/output/20230922-1601eco/simulation.log",
+                data=b"Simulation OK",
+            )
+    elif scenario == "nominal_results":
         with zipfile.ZipFile(
             out_path,
             mode="w",
@@ -24,6 +38,10 @@ def create_final_zip(study: StudyDTO, *, scenario: str = "nominal") -> str:
             zf.writestr("simulation.log", data=b"Simulation OK")
     elif scenario == "corrupted":
         out_path.write_bytes(b"PK corrupted content")
+    elif scenario == "missing":
+        pass
+    else:
+        raise NotImplementedError(scenario)
     return str(out_path)
 
 
@@ -68,12 +86,39 @@ class TestFinalZipExtractor:
         assert not finished_study.final_zip_extracted
 
     @pytest.mark.unit_test
-    def test_extract_final_zip__finished_study__nominal(self, finished_study: StudyDTO) -> None:
+    def test_extract_final_zip__finished_study__nominal_study(self, finished_study: StudyDTO) -> None:
         display = mock.Mock(spec=DisplayTerminal)
         display.generate_progress_bar = lambda names, *args, **kwargs: names
 
         # Prepare a valid final ZIP
-        finished_study.local_final_zipfile_path = create_final_zip(finished_study)
+        finished_study.local_final_zipfile_path = create_final_zip(finished_study, scenario="nominal_study")
+
+        # Initialize and execute the ZIP extraction
+        extractor = FinalZipExtractor(display=display)
+        extractor.extract_final_zip(finished_study)
+
+        # Check the result
+        display.show_message.assert_called_once()
+        display.show_error.assert_not_called()
+
+        assert finished_study.final_zip_extracted
+        assert not finished_study.with_error
+
+        result_dir = Path(finished_study.output_dir).joinpath(finished_study.name)
+        expected_files = [
+            "input/study.antares",
+            "output/20230922-1601eco/simulation.log",
+        ]
+        for file in expected_files:
+            assert result_dir.joinpath(file).is_file()
+
+    @pytest.mark.unit_test
+    def test_extract_final_zip__finished_study__nominal_results(self, finished_study: StudyDTO) -> None:
+        display = mock.Mock(spec=DisplayTerminal)
+        display.generate_progress_bar = lambda names, *args, **kwargs: names
+
+        # Prepare a valid final ZIP
+        finished_study.local_final_zipfile_path = create_final_zip(finished_study, scenario="nominal_results")
 
         # Initialize and execute the ZIP extraction
         extractor = FinalZipExtractor(display=display)
@@ -127,8 +172,11 @@ class TestFinalZipExtractor:
         assert not finished_study.final_zip_extracted
         assert finished_study.with_error
 
-        result_dir = Path(finished_study.local_final_zipfile_path).with_suffix("")
-        assert not result_dir.joinpath("simulation.log").exists()
+        result_dirs = [
+            Path(finished_study.output_dir).joinpath(finished_study.name),
+            Path(finished_study.local_final_zipfile_path).with_suffix(""),
+        ]
+        assert not any(result_dir.exists() for result_dir in result_dirs)
 
     @pytest.mark.unit_test
     def test_extract_final_zip__finished_study__corrupted(self, finished_study: StudyDTO) -> None:
@@ -149,5 +197,8 @@ class TestFinalZipExtractor:
         assert not finished_study.final_zip_extracted
         assert finished_study.with_error
 
-        result_dir = Path(finished_study.local_final_zipfile_path).with_suffix("")
-        assert not result_dir.joinpath("simulation.log").exists()
+        result_dirs = [
+            Path(finished_study.output_dir).joinpath(finished_study.name),
+            Path(finished_study.local_final_zipfile_path).with_suffix(""),
+        ]
+        assert not any(result_dir.exists() for result_dir in result_dirs)
