d45add46fb3df8cb0471b306b20c05b1399d9841
belthlemar <martin.belthle@rte-france.com>
2026-06-09 14:54:24 +0200

    feat: support oversubscribe option

--- antareslauncher/remote_environnement/slurm_script_features.py
+++ antareslauncher/remote_environnement/slurm_script_features.py
@@ -16,6 +16,7 @@ class ScriptParametersDTO:
     run_mode: Modes
     post_processing: bool
     other_options: str
+    over_subscribe: bool
 
 
 class SlurmScriptFeatures:
@@ -78,6 +79,8 @@ class SlurmScriptFeatures:
 
         # Construct the `sbatch` command
         args = ["sbatch"]
+        if script_params.over_subscribe:
+            args.append("--oversubscribe")
         args.extend(f"{k}={shlex.quote(str(v))}" for k, v in _opts.items() if v)
         args.extend(
             shlex.quote(arg)
